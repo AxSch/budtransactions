b7da442862f2e2470de85dfae9fff0bb920bd803
AxSch <alex_schuneman@outlook.com>
2021-01-30 20:43:14 +0000

    Utilize Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header/Header';
 import Transactions from './Pages/Transactions/Transactions';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Transactions />
     </div>
   );
