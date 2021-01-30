c97bcd74098ec45eef903061c9fb37290d9286eb
AxSch <alex_schuneman@outlook.com>
2021-01-30 20:42:38 +0000

    Add Header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <h1>Transactions</h1>
+            </div>
+            <div>
+                <span>Edit</span>
+            </div>
+        </div>
+    )
+}
+
+export default Header
