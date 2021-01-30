69fe66c873baa97d4b7bc8bc8e730d37c371d7d6
AxSch <alex_schuneman@outlook.com>
2021-01-30 20:12:59 +0000

    Refactor to correct .map error & unique keys

--- src/Components/TransactionList/TransactionList.js
+++ src/Components/TransactionList/TransactionList.js
@@ -5,7 +5,11 @@ const TransactionList = ({ transactions }) => {
     return (
         <div>
             {transactions.map(transaction => {
-                <div>{transaction.name}</div>
+                return (
+                    <div key={transaction.id}>
+                        {transaction.description}
+                    </div>
+                )
             })}
         </div>
     )
