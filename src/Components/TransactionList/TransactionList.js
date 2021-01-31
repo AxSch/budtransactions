f7775388fc6665ea816f28f595a7142334edf645
AxSch <alex_schuneman@outlook.com>
2021-01-31 23:54:35 +0000

    Refactor & more styling to button
    Added selectTransactions4Removal() to use for conditional render

--- src/Components/TransactionList/TransactionList.js
+++ src/Components/TransactionList/TransactionList.js
@@ -1,17 +1,45 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components'
-import { removeTransactions, selectIsEdit } from '../../reducers/transactions/transactionsSlice'
+import { removeTransactions, selectIsEdit, selectTransactions4Removal } from '../../reducers/transactions/transactionsSlice'
 import Transaction from '../Transaction/Transaction'
 
 const StyledTransactions = styled.div`
     position: absolute;
+    top: 140px;
     width: 100%;
+`
+
+const StyledRemoveButton = styled.button`
+    background-color: rgb(246, 0, 0);
+    padding: 20px;
+    border: none;
+    width: 100%;
+    font-size: 16px;
+    color: rgb(255,255,255);
+    @media(min-width: 640px) {
+        width: 50%;
+    }
+    @media(min-width: 1024px) {
+        width: 40%;
+    }
+`
+
+const StyledButtonContainer = styled.div`
+    display: flex;
+    position: fixed;
+    width: 100%;
+    justify-content: center;
+    bottom: 0;
+    padding-top: 60px;
+    z-index: 4;
 `
 
 const TransactionList = ({ transactions }) => {
     const dispatch = useDispatch()
     const isEdit = useSelector(selectIsEdit)
+    const transactions4Removal = useSelector(selectTransactions4Removal)
+
     const removeButton = () => {
         dispatch(removeTransactions())
     }
@@ -22,9 +50,13 @@ const TransactionList = ({ transactions }) => {
                     <Transaction key={transaction.id} transaction={transaction} />
                 )
             })}
-            {isEdit ? <button onClick={() => removeButton()}>
-                Remove
-            </button> : null}
+            {isEdit && transactions4Removal.length > 0 ? (
+                <StyledButtonContainer>
+                    <StyledRemoveButton onClick={() => removeButton()}>
+                        Remove
+                    </StyledRemoveButton>
+                </StyledButtonContainer>
+            ) : null}
         </StyledTransactions>
     )
 }
